41d2aec3de69bb4a07eea1d577e11d1adbf5cf95
maulidacy <cahyamaulida011@gmail.com>
2026-05-07 19:09:11 +0700

    add project detail

---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,9 @@
+import { PortfolioHero } from "@/components/home/sections/portfolio-hero";
+
+export default function PortfolioPage() {
+  return (
+    <main className="bg-white">
+      <PortfolioHero />
+    </main>
+  );
+}
